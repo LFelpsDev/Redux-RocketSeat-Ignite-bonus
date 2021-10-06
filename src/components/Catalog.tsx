899c8818b591bd55d06a3c3febe82774b4c99949
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-10-05 21:05:39 -0300

    :zap: Trabalhando com Actions

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,15 +1,22 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
+import { useDispatch } from "react-redux";
 import api from "../services/api";
+import { addProductToCart } from "../store/modules/cart/actions";
 import { IProduct } from "../store/modules/cart/types";
 
 export default function Catalog() {
+  const dispatch = useDispatch()
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
     api.get('/products').then(response => {
       setCatalog(response.data)
     })
-  }, [])
+  }, []);
+
+  const handleAddProductToCart = useCallback((product: IProduct) => {
+    dispatch(addProductToCart(product))
+  }, [dispatch])
 
 
 
@@ -21,7 +28,10 @@ export default function Catalog() {
           <strong>{product.title}</strong> {" - "}
           <span>{product.price}</span> {" - "}
 
-          <button type="button">Comprar</button>
+          <button 
+          onClick={() => handleAddProductToCart(product)}
+          type="button"
+          >Comprar</button>
         </article>
       ))}
     </main>
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -5,7 +5,9 @@ const INITIAL_STATE = {
   items: []
 }
 
-const cart: Reducer<ICartState> = () => {
+const cart: Reducer<ICartState> = (state, action) => {
+  console.log(state);
+  console.log(action);
   return INITIAL_STATE 
 }
 
